223fbe51c2a23c3e39588b43408c678867b04c39
Bilkis Ismail <bilkisismail07@gmail.com>
2023-06-23 08:00:30 +0700

    fix cloudinary

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary");
 
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "infomediaon5",
+  api_key: "349237237622967",
+  api_secret: "j_WjXliM4n4DA442p86LZu0WZz8",
 });
 
 module.exports = cloudinary;
